d28ca79e143ce678c6f64b8113f815b35c9ad8d3
Josephine180 <pierrejosephine09@gmail.com>
2025-06-02 16:16:31 +0200

    add CORS and API

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,47 @@
-// On importe le module 'http' de Node.js, qui permet de créer un serveur web
 const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT ||'3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
 
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
-// On démarre le serveur et on lui demande d'écouter sur un port
-// process.env.PORT est utilisé pour permettre au serveur de fonctionner sur des plateformes cloud (comme Heroku)
-// Si cette variable d'environnement n'est pas définie, on utilise le port 3000 par défaut
-server.listen(process.env.PORT || 3000);
+server.listen(port);
