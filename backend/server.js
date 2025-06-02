1759cce645b63536b9312183ad9a8aba858c9edf
Josephine180 <pierrejosephine09@gmail.com>
2025-06-02 15:30:53 +0200

    add widdlewares

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,10 @@
 // On importe le module 'http' de Node.js, qui permet de créer un serveur web
 const http = require('http');
+const app = require('./app');
 
-// On crée un serveur HTTP avec la méthode createServer
-// Cette méthode prend une fonction de rappel (callback) qui sera appelée à chaque requête reçue
-// 'req' représente la requête envoyée par le client
-// 'res' représente la réponse que le serveur renvoie
-const server = http.createServer((req, res) => {
-  // On termine la réponse en envoyant un message au client
-  // Cela signifie que chaque fois que quelqu'un accède au serveur, il verra ce message
-  res.end('Voilà la réponse du server');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
 
 // On démarre le serveur et on lui demande d'écouter sur un port
 // process.env.PORT est utilisé pour permettre au serveur de fonctionner sur des plateformes cloud (comme Heroku)
